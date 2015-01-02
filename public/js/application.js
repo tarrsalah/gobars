f4e1bb903efd884cac54813f21e68880e59ae09d
tarrsalah <tarrsalah@gmail.com>
2015-01-02 06:32:22 +0100

    list bars.

--- public/js/application.js
+++ public/js/application.js
@@ -5,8 +5,27 @@
 
     window.App= App;
 
-    App.Store = DS.Store.extend({
-	adapter: DS.RESTAdapter
+    App.ApplicationAdapter = DS.RESTAdapter.extend({
+	host: 'http://localhost:3000'
+    });
+
+    App.ApplicationSerializer = DS.ActiveModelSerializer.extend({
+	primaryKey: 'id'
+    });
+
+    App.Bar = DS.Model.extend({
+	name: DS.attr('string')
+    });
+
+
+    App.ApplicationRoute = Ember.Route.extend({
+	model: function() {
+	    var bars = this.store.find('bar');
+	    bars.then(function(bars) {
+		console.log(bars.content); // I'm getting an empty array !
+	    });
+	    return bars;
+	}
     });
 
     App.ApplicationController = Ember.Controller.extend({
