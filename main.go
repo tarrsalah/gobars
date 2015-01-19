cd204288572251a2d784f75b47f47a8d71201e27
tarrsalah <tarrsalah@gmail.com>
2015-01-19 01:40:14 +0100

    decouple from gohttp:
    replace gohttp/app by bmizerany/pat, and use
    tarrsalah/jsonds from handing json marshaling
    and Unmarshaling.

--- main.go
+++ main.go
@@ -2,15 +2,35 @@ package main
 
 import (
 	"database/sql"
-	"encoding/json"
-	"github.com/gohttp/app"
+	"github.com/bmizerany/pat"
 	"github.com/gohttp/logger"
-	"github.com/gohttp/response"
 	_ "github.com/mattn/go-sqlite3"
+	"github.com/tarrsalah/jsonds"
 	"log"
 	"net/http"
 	"os"
 )
+
+var (
+	ctx *Context
+)
+
+func init() {
+	var (
+		db  *sql.DB
+		err error
+	)
+
+	os.Remove("./bars.db")
+	if db, err = sql.Open("sqlite3", BARS_FILE); err != nil {
+		panic(err)
+	}
+	if _, err = db.Exec(SQL); err != nil {
+		panic(err)
+	}
+
+	ctx = NewContext(db)
+}
 
 const (
 	BARS_FILE = "bars.db"
@@ -25,26 +45,6 @@ type Context struct {
 	DS
 }
 
-func WrapBars(bars []Bar) map[string][]Bar {
-	return map[string][]Bar{
-		"bars": bars,
-	}
-}
-
-func WrapBar(bar Bar) map[string]Bar {
-	return map[string]Bar{
-		"bar": bar,
-	}
-}
-
-func UnWrapBar(w map[string]Bar) Bar {
-	return w["bar"]
-}
-
-func UnWrapBars(w map[string][]Bar) []Bar {
-	return w["bars"]
-}
-
 func NewContext(db *sql.DB) *Context {
 	return &Context{
 		DS: DS{
@@ -54,50 +54,53 @@ func NewContext(db *sql.DB) *Context {
 }
 
 func main() {
-	var (
-		db  *sql.DB
-		err error
-	)
-
-	os.Remove("./bars.db")
-	if db, err = sql.Open("sqlite3", BARS_FILE); err != nil {
-		panic(err)
-	}
-	if _, err = db.Exec(SQL); err != nil {
-		panic(err)
-	}
-
-	ctx := NewContext(db)
-	app := app.New()
-	app.Use(logger.New())
+	logger := logger.New()
+	pat := pat.New()
 
 	fs := http.FileServer(http.Dir("./public/"))
+
 	http.Handle("/public/", http.StripPrefix("/public/", fs))
-
-	app.Get("/", func(w http.ResponseWriter, r *http.Request) {
-		http.ServeFile(w, r, "./public/index.html")
-	})
-
-	app.Get("/bars", func(w http.ResponseWriter, r *http.Request) {
-		response.OK(w, WrapBars(ctx.GetAllBars()))
-	})
-
-	app.Post("/bars", func(w http.ResponseWriter, r *http.Request) {
-		dec := json.NewDecoder(r.Body)
-		bar := Bar{}
-		wrapped := WrapBar(bar)
-
-		if err := dec.Decode(&wrapped); err != nil {
-			log.Fatal(err)
-			return
-		}
-
-		bar = UnWrapBar(wrapped)
-		id, _ := ctx.InsertBar(bar)
-		bar.Id = id
-		response.Created(w, WrapBar(bar))
-	})
+	pat.Get("/", logger(http.HandlerFunc(indexHandler)))
+	pat.Get("/bars", logger(http.HandlerFunc(getBarsHandler)))
+	pat.Post("/bars", logger(http.HandlerFunc(postBarHandler)))
 
 	log.Println("Listening on http://localhost:3000 ...")
-	app.Listen(":3000")
+	http.Handle("/", pat)
+	err := http.ListenAndServe(":3000", nil)
+	if err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
+}
+
+func getBarsHandler(w http.ResponseWriter, r *http.Request) {
+	replyJsonDs(w, ctx.GetAllBars(), http.StatusOK)
+}
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	http.ServeFile(w, r, "./public/index.html")
+}
+func postBarHandler(w http.ResponseWriter, r *http.Request) {
+	bar := Bar{}
+	if err := jsonds.NewDecoder(r.Body).Decode(&bar); err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+	id, _ := ctx.InsertBar(bar)
+	bar.Id = id
+	replyJsonDs(w, bar, http.StatusOK)
+}
+
+func replyJsonDs(w http.ResponseWriter, v interface{}, code int) {
+	var (
+		b   []byte
+		err error
+	)
+	w.Header().Set("Content-Type", "application/json")
+	if b, err = jsonds.Marshal(v); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	} else {
+		w.WriteHeader(http.StatusOK)
+		w.Write(b)
+	}
 }
