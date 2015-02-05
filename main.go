63ceb1df57e74048767bb40324afbedae8b9dd48
tarrsalah <tarrsalah@gmail.com>
2015-02-05 22:07:23 +0100

    one file.

--- main.go
+++ main.go
@@ -1,12 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"github.com/gohttp/logger"
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
 	"log"
 	"net/http"
+	"os"
 )
 
 const (
@@ -18,6 +20,73 @@ const (
 		);
 		`
 )
+
+type Bar struct {
+	Id   int64  `json:"id"`
+	Name string `json:"name"`
+}
+
+func (b Bar) Root() string {
+	return "bar"
+}
+
+type Bars []Bar
+
+func (b Bars) Root() string {
+	return "bars"
+}
+
+type store struct {
+	db *sql.DB
+}
+
+func NewStore() *store {
+	var (
+		db  *sql.DB
+		err error
+	)
+
+	os.Remove("./bars.db")
+	if db, err = sql.Open("sqlite3", BARS_FILE); err != nil {
+		panic(err)
+	}
+	if _, err = db.Exec(SQL); err != nil {
+		panic(err)
+	}
+
+	return &store{db}
+}
+
+func (b *store) GetAllBars() Bars {
+	var (
+		id   int64
+		name string
+	)
+	bars := []Bar{}
+
+	rows, err := b.db.Query("select * from bars")
+	if err != nil {
+		panic(err)
+	}
+	defer rows.Close()
+
+	for rows.Next() {
+		if err := rows.Scan(&id, &name); err != nil {
+			panic(err)
+		}
+		bars = append(bars, Bar{id, name})
+	}
+	return Bars(bars)
+}
+
+func (b *store) InsertBar(bar Bar) (int64, error) {
+
+	if result, err := b.db.Exec("insert into bars(bar) values(?)", bar.Name); err != nil {
+		panic(err)
+	} else {
+		return result.LastInsertId()
+	}
+}
 
 var s = NewStore()
 
